1a3e9ba1ef9dd3039b43a30c05295a4f02ad39a8
Jeremy Lorelli <lorelli@slac.stanford.edu>
2026-03-11 13:36:44 -0700

    Skip ParticleGroup variables, as these will never be served by EPICS

--- lume_pva/runner.py
+++ lume_pva/runner.py
@@ -1,5 +1,5 @@
 from lume.model import LUMEModel, Variable
-from lume.variables import Variable
+from lume.variables import Variable, ParticleGroup
 from p4p.server import ServerOperation
 from p4p.server.thread import SharedPV
 from p4p.client.thread import Subscription, Disconnected, RemoteError, Cancelled
@@ -168,6 +168,8 @@ class Runner:
 
             handler = find_variable_handler(type(var))
             if handler is None:
+                if type(var) is ParticleGroup:
+                    continue # ParticleGroupVariable is a special case that doesn't have a handler
                 raise RuntimeError(f'Unknown type "{type(var)}"')
 
             # Cache handler and type for later
